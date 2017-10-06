c4123745372704de5669fce9b8477c9c5c9550c3
JBesserer <31408090+JBesserer@users.noreply.github.com>
2017-10-06 11:33:25 -0400

    Friday Jeremy
    Retour de reservation

--- app/views/returnReservation.php
+++ app/views/returnReservation.php
@@ -1,12 +1,12 @@
 <?php
 /****************************************************************
-		File : modifyReservation.php
+		File : returnReservation.php
 		Authour : Jérémy Besserer-Lemay
-		Functionality : Page to modify a vehicule's reservation
-			Date: 2017-10-03
+		Functionality : User page to return a reservation after being done with it
+			Date: 2017-10-06
 			
 			Last modification:
-			2017-10-03     Jérémy Besserer-Lemay   1 Creation
+			2017-10-06     Jérémy Besserer-Lemay   1 Creation
  ******************************************************************/
 session_start();
 error_reporting(0);
@@ -20,7 +20,7 @@ $gReservation = new InfoReservation();
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         
-	<title>Modification de reservation</title>
+	<title>Retour de réservation</title>
         
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
         <meta name="viewport" content="width=device-width" />
@@ -112,7 +112,7 @@ $gReservation = new InfoReservation();
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-						<a class="navbar-brand" href="#">Gestionnaire de réservations</a>
+						<a class="navbar-brand" href="#">Retour de véhicule</a>
 					</div>
 					<div class="collapse navbar-collapse">
 						<ul class="nav navbar-nav navbar-right">
@@ -152,33 +152,34 @@ $gReservation = new InfoReservation();
 	                <div class="row">
 	                    <div class="col-md-12">
 	                        <div class="card">
-	                            <div class="card-header" data-background-color="blue">
-	                                <h4 class="title">Simple Table</h4>
-	                                <p class="category">Here is a subtitle for this table</p>
-	                            </div>
-	                            <div class="card-content table-responsive">
-	                                <table class="table" id="example">
-	                                    <thead class="text-primary">
-	                                    	<th>ID Réservation</th>
-	                                    	<th>Vehicule</th>
-	                                    	<th>Réservé par</th>
-						<th>Date de début</th>
-                                                <th>Date de retour prévu</th>
-	                                    </thead>
-	                                    <tbody>
-                                                <?php $gReservation->getListReservations(); ?>
-	                                    </tbody>
-	                                </table>
-	                            </div>
+                                    <form action="../controllers/cReservation.php" method="post" enctype="multipart/form-data">
+                                        <fieldset>
+                                            <div class="col-md-6">
+                                                <label for='selectReservation'>
+                                                Véhicule a retourner:
+                                                </label>
+                                                <?php $gReservation->getSelectReservations($_SESSION['user']['pk_utilisateur']); ?>
+                                            </div>
+
+
+                                            <div class="col-md-6">
+                                                <label for='odometer'>
+                                                Kilométrage du véhicule:
+                                                </label>
+                                                <br>
+                                                <input type="text" size="40" maxlenght="50" class="form-control" id="odometer" name="odometer" placeholder="Kilométrage au retour" required>
+                                            </div>
+                                            <div class=" col-md-12 buttons">
+                                                <div class="centerbuttons">
+                                                    <button class="btn btn-default" type="submit" >Envoyer</button> 
+                                                    <button class="btn btn-default" type="button" id="Retour" name="Retour">Retour</button>
+                                                </div>
+                                            </div>
+                                        </fieldset>
+                                    </form>
 	                        </div>
 	                    </div>
-                            <div class="buttons">
-                                <div class="centerbuttons">
-                                    <button class="btn btn-default" name="Ajouter" id="Ajouter">Ajouter</button>
-                                    <button class="btn btn-default" name="Modifier" id="Modifier">Modifier</button>
-                                    <button class="btn btn-default" name="Consulter" id="Consulter">Consulter</button>
-                                </div>
-                            </div>
+                            
 	                </div>
 	            </div>
 	        </div>
@@ -219,7 +220,6 @@ $gReservation = new InfoReservation();
     
         
         <script src="../js/jquery-3.1.0.min.js" type="text/javascript"></script>
-        <script src="../js/jquery.dataTables.min.js"></script> 
 	
 	<script src="../js/bootstrap.min.js" type="text/javascript"></script>
 	<script src="../js/material.min.js" type="text/javascript"></script>
@@ -241,57 +241,8 @@ $gReservation = new InfoReservation();
 
 	<script type="text/javascript">
     	$(document).ready(function(){
-
-			// Javascript method's body can be found in assets/js/demos.js
-        	demo.initDashboardPageCharts();
-                
-                var table = $('#example').DataTable();
-                
-                //modifie les styles pour la sélection de rangée 
-                $('#example tbody').on('click', 'tr', function () {
-                    if ($(this).hasClass('selected')) {
-                        $(this).removeClass('selected');
-                    } else {
-                        table.$('tr.selected').removeClass('selected');
-                        $(this).addClass('selected');
-                    }
-                });
-
-                //clic modifier, envoie en get le id selectionné
-                $('#Modifier').click(function () {
-
-                    if ($('#example tr.selected td:first').length > 0) {
-                        var idcont = $('#example tr.selected td:first').html();
-                        window.location.href = "http://localhost/Presentation/VueModifierClient?client=" + idcont + "";
-                    }else{
-                        swal({
-                                title:"",
-                                text:"Vous devez sélectionner un client.",
-                                type:"warning",
-                                allowOutsideClick : true
-                            });
-                    }
-                });
-
-                //clic consulter, envoie en get le id selectionné
-                $('#Consulter').click(function () {
-                    if ($('#example tr.selected td:first').length > 0) {
-                        var idcons = $('#example tr.selected td:first').html();
-                        window.location.href = "http://localhost/Presentation/VueConsulterClient?client=" + idcons + "";
-                    }else{
-                        swal({
-                                title:"",
-                                text:"Vous devez sélectionner un client.",
-                                type:"warning",
-                                allowOutsideClick : true
-                            });
-                    }
-                });
-
-                //clic ajouter
-                $('#Ajouter').click(function () {
-                    window.location.href = "http://localhost/Presentation/VueAjouterClient";
-                });
+            // Javascript method's body can be found in assets/js/demos.js
+            demo.initDashboardPageCharts();
 
     	});
 	</script>
